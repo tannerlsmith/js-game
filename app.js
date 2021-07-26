5a6f35188638b6a92e31138d864b569fce3ff1c1
tannerlsmith <tanner.smith@uvm.edu>
2021-07-26 09:10:57 -0400

    added fall, jump functions, moving platforms

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
     let isGameOver = false
+    let platformCount = 5
+    let platforms = []
+    let upTimerId 
+    let downTimerId
 
     function createDoodler() {
         // puts doodler inside grid 
@@ -13,19 +17,89 @@ document.addEventListener('DOMContentLoaded', () => {
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
 // ========================================== //
-// left off here, 12:11 (line 18)
-// https://www.youtube.com/watch?v=8xPsg6yv7TU&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=3
-    function createPlatforms() {}
+
+
+    // constructor
+    class Platform {
+        constructor(newPlatBottom) {
+            // each has its own bottom
+            this.bottom = newPlatBottom
+            // will appear in grid
+            this.left = Math.random() * 315 
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
+    }
+
+    function createPlatforms() {
+        for (let i = 0; i < platformCount; i++) {
+            // divides by the space of plat-Count (5)
+            let platGap = 600 / platformCount
+            // increments the gap space
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform(newPlatBottom)
+            platforms.push(newPlatform)
+            console.log(platforms)
+
+        }
+    }
+
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+                platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
+
+            }) 
+        }
+    }
     
+    function jump() {
+        clearInterval(downTimerId)
+        upTimerId = setInterval(function () {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace > 350) {
+                fall()
+            }
+        },30)
+    }
+
+
+    function fall() {
+        clearInterval(upTimerId)
+        downTimerId = setInterval(function () {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+        })
+    }
+
+
+
     // doodler appears if this function is evoked
     function start() {
         // if game is NOT over, we create doodler.
         if (!isGameOver) {
             createDoodler()
             createPlatforms()
+            setInterval(movePlatforms, 30)
+            jump()
 
         }
     }
     // attach to button later.
     start()
 })
+
+
+
+
+
+// https://www.youtube.com/watch?v=8xPsg6yv7TU&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=3
+// 27:16, line 80 if (doodlerBottomSpace)
